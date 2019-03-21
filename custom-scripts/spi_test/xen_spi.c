9832056dfd1ce9f0a5d62a35f52e18c9aa58b8ad
Carlos Moratelli <crmoratelli@gmail.com>
2019-03-21 11:57:50 -0300

    Improve spi application.

--- custom-scripts/spi_test/xen_spi.c
+++ custom-scripts/spi_test/xen_spi.c
@@ -9,28 +9,20 @@
 #include <rtdm/uapi/spi.h>
 #include <math.h>
 
-#define CHANNEL(x) (unsigned char)(x << 4)
-
-static const char *device = "/dev/rtdm/spi32766/slave32766.0";
+#define DEVICE_SPI "/dev/rtdm/spi32766/slave32766.0"
 #define PIN_NAME_PWM "/dev/rtdm/gpiopwm0"
 
 #define TRANSFER_SIZE 6
 #define CLOCK_RES 1e-9 /* Clock resolution is 1ns */
 #define DIV_TO_MS  1000000.0
 #define DIV_TO_US  1000.0
-#define LOOP_PERIOD 100000000 /* Expressed in ticks - 100 ms */
+#define LOOP_PERIOD 100000 /* Expressed in ticks - 100 us */
 
-struct rtdm_spi_config config = {3000000, 0, 8};
 struct rtdm_spi_iobufs iobufs;
-void *p;
-static unsigned char *i_area, *o_area;
-int fd;
+volatile int finish = 1;
 
-RT_TASK loop_task;
-
-/* */
+/* Power model data */
 const double coef_nom[4] = {0.2039,   -0.4210,    0.2811,   -0.0608};
-
 const double coef_den[4] = {1.0000,   -1.6919,    0.3841,    0.3080};
 
 double y = 0; //Valor mensurado pelo sensor.
@@ -45,13 +37,21 @@ double e_n1 = 0; //Sinal de erro de uma amostra atrasada.
 double e_n2 = 0; //Sinal de erro de duas amostras atrasada.
 double e_n3 = 0; //Sinal de erro de tres amostras atrasada.
 
-static struct gpiopwm config_pwm;// = {988, 50, 0, 1000000, 1000000};
+/* Statistics */
+#define TABLE_SIZE 100
+double loop_info[100][6];
+double time_cases[4] = {9999999, -1, 9999999, -1};
+#define FASTEST_LOOP 0
+#define SLOWER_LOOP 1
+#define BETTER_JITTER 2
+#define WORST_JITTER 3
 
 
 double map(double x, double in_min, double in_max, double out_min, double out_max){
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+/* Perform power model equation. */
 int do_control(int v, unsigned int t){
 	double u, ref;
 
@@ -74,53 +74,57 @@ int do_control(int v, unsigned int t){
 	return (int)u;
 }
 
+/* Configure the PWM controller. */
+int configure_pwm_pin(){
+	struct gpiopwm config_pwm;
+	int fd;
+	void *p;
+	int ret;
+	
 
-/* Do bitbang on gpio pins.*/
-void loop_task_proc(void *arg){
-	RT_TASK *curtask;
-	RT_TASK_INFO curtaskinfo;
-	int iret = 0;
-	int value = 1;
-	int pwm;
-	int fdpwm;
+	config_pwm.gpio = 988;
+	config_pwm.duty_cycle =	0;
+	config_pwm.range_min = 0;
+	config_pwm.range_max = 1000;
+	config_pwm.period =	1000000;
 
-	RTIME tstart, tlast=0, now;
-
-	curtask = rt_task_self();
-	rt_task_inquire(curtask, &curtaskinfo);
-	int ctr = 0;
-
-	config_pwm.gpio = 988
-	config_pwm.duty_cycle =	atoi(argv[4]);
-	config_pwm.range_min = atoi(argv[5]);
-	config_pwm.range_max = atoi(argv[6]);
-	config_pwm.period =	atoi(argv[7]);
-
-
-	fdpwm = open(PIN_NAME_PWM, O_RDWR);
-	if(fdpwm < 0){
+	fd = open(PIN_NAME_PWM, O_RDWR);
+	if(fd < 0){
 		perror("open: ");
 		exit(0);
 	}
 
-	iret = ioctl(fdpwm, GPIOPWM_RTIOC_SET_CONFIG, &config_pwm);
-	if (iret){
-		printf("%d, %s", iret, "failed to set config");
+	ret = ioctl(fd, GPIOPWM_RTIOC_SET_CONFIG, &config_pwm);
+	if (ret){
+		printf("%d, %s", ret, "failed to set pwm config.");
+		exit(0);
 	}
 
-	ioctl(fdpwm, GPIOPWM_RTIOC_START);
+	ret = ioctl(fd, GPIOPWM_RTIOC_START);
+	if (ret){
+		printf("%d, %s", ret, "failed to start pwm.");
+		exit(0);
+	}
 
-	fd = open(device, O_RDWR);
+	return fd;
+}
+
+/* Configure the SPI controller. */
+int configure_spi_device(unsigned char **i_area, unsigned char **o_area){
+	struct rtdm_spi_config config = {3600000, 0, 8};
+	int fd;
+	void *p;
+	int ret;
+
+	fd = open(DEVICE_SPI, O_RDWR);
 	if (fd < 0){
-		perror("open():");
+		perror("open(spi):");
 	}
 
-	/*
-	 * spi mode
-	 */
-	iret = ioctl(fd, SPI_RTIOC_SET_CONFIG, &config);
-	if (iret == -1){
-		perror("ioctl():");
+	 /* spi mode  */
+	ret = ioctl(fd, SPI_RTIOC_SET_CONFIG, &config);
+	if (ret == -1){
+		printf("ioctl(spi) failed %d\n", ret);
 	}
 
 	printf("spi mode: %d\n", config.mode);
@@ -129,67 +133,150 @@ void loop_task_proc(void *arg){
 
 	iobufs.io_len = TRANSFER_SIZE;
 
-	iret = ioctl(fd, SPI_RTIOC_SET_IOBUFS, &iobufs);
-	if(iret < 0){
+	ret = ioctl(fd, SPI_RTIOC_SET_IOBUFS, &iobufs);
+	if(ret < 0){
 		perror("ioctl()");
 	}
 
 	p = mmap(NULL, iobufs.io_len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 	if (p == MAP_FAILED){
-		return;
+		printf("mmap() failed. \n");
+		exit(0);
 	}
 
-	i_area = p + iobufs.i_offset;
-	o_area = p + iobufs.o_offset;
+	*i_area = p + iobufs.i_offset;
+	*o_area = p + iobufs.o_offset;
 	
 	printf("input_area[%u..%u], output_area[%u..%u], mapping length=%u\n",
 		     iobufs.i_offset, iobufs.i_offset + TRANSFER_SIZE - 1,
 		     iobufs.o_offset, iobufs.o_offset + TRANSFER_SIZE - 1,
 		     iobufs.io_len);
 
-	//Print the info
-	printf("Starting task %s with period of %.2f ms ....\n", curtaskinfo.name, LOOP_PERIOD/1000000);
+	return fd;
+}
 
-	//Make the task periodic with a specified loop period
-	rt_task_set_periodic(NULL, TM_NOW, LOOP_PERIOD);
 
-	o_area[0] = 0x1;
-	o_area[1] = 0x80;
-	o_area[2] = 0;
+/* Update statistic tables */
+void update_statistics(int ctr, double passed_time, double loop_time, double jitter, int value, int pwm){
+	loop_info[ctr%TABLE_SIZE][0] = ctr;
+	loop_info[ctr%TABLE_SIZE][1] = passed_time;
+	loop_info[ctr%TABLE_SIZE][2] = loop_time;
+	loop_info[ctr%TABLE_SIZE][3] = jitter;
+	loop_info[ctr%TABLE_SIZE][4] = (double)value;
+	loop_info[ctr%TABLE_SIZE][5] = (double)pwm;
 
-	tstart = rt_timer_read();
-
-	//Start the task loop
-	while(1){
-		now = rt_timer_read();
-		ctr++;
-
-		iret =ioctl(fd, SPI_RTIOC_TRANSFER);
-		if(iret < 0){
-			perror("ioctl");
+	if (ctr > 50){
+		if(time_cases[FASTEST_LOOP] > loop_info[ctr%TABLE_SIZE][2]){
+			time_cases[FASTEST_LOOP] = loop_info[ctr%TABLE_SIZE][2];
 		}
 
-		value = (i_area[1] & 0x7) << 8 | i_area[2];
-		printf("value: %d\n", value);
+		if(time_cases[SLOWER_LOOP] < loop_info[ctr%TABLE_SIZE][2]){
+			time_cases[SLOWER_LOOP] = loop_info[ctr%TABLE_SIZE][2];
+		}
 
-		pwm = do_control(value, (rt_timer_read() - tstart));
-		printf("pwm: %d\n", pwm);
+		if(time_cases[BETTER_JITTER] > loop_info[ctr%TABLE_SIZE][3]){
+			time_cases[BETTER_JITTER] = loop_info[ctr%TABLE_SIZE][3];
+		}
 
-		ioctl(fd, GPIOPWM_RTIOC_CHANGE_DUTY_CYCLE, pwm);
-
-		printf("Loop count: %d, run time: %.2f ms, loop time: %.2f us jitter %.5f us\n", ctr, (now - tstart)/DIV_TO_MS, (rt_timer_read()-now)/DIV_TO_US, ((long int)now - (long int)tlast - LOOP_PERIOD)/DIV_TO_US);
-		tlast = now;		
-
-		rt_task_wait_period(NULL);
+		if(time_cases[WORST_JITTER] < loop_info[ctr%TABLE_SIZE][3]){
+			time_cases[WORST_JITTER] = loop_info[ctr%TABLE_SIZE][3];
+		}
 	}
 }
 
 
+
+/* Real-time Task */
+void loop_task_proc(void *arg){
+	RT_TASK *curtask;
+	RT_TASK_INFO curtaskinfo;
+	RTIME tstart, tlast=0, now;
+	int ret = 0;
+	int value = 1;
+	int pwm, old_pwm;
+	int fdpwm, fdspi;
+	int ctr = 0;
+	unsigned char *i_area, *o_area;
+
+	curtask = rt_task_self();
+	rt_task_inquire(curtask, &curtaskinfo);
+
+	fdpwm = configure_pwm_pin();
+
+	fdspi = configure_spi_device(&i_area, &o_area);
+
+	printf("Starting task %s with period of %d us ....\n", curtaskinfo.name, LOOP_PERIOD/1000);
+
+	/* Make the task periodic with a specified loop period */
+	rt_task_set_periodic(NULL, TM_NOW, LOOP_PERIOD);
+
+	/* Data to send to the MCP3008 */
+	o_area[0] = 0x1;
+	o_area[1] = 0x80;
+	o_area[2] = 0;
+
+	/* Initial time. */
+	tstart = rt_timer_read();
+
+	while(finish){
+		now = rt_timer_read();
+		ctr++;
+
+		/* Data tranfer to the MPC3008 */
+		ret =ioctl(fdspi, SPI_RTIOC_TRANSFER);
+		if(ret < 0){
+			perror("ioctl");
+		}
+
+		/* Read the returned data. */
+		value = (i_area[1] & 0x7) << 8 | i_area[2];
+
+		/* Performe the control model equations. */
+		pwm = do_control(value, (rt_timer_read() - tstart));
+
+		/* Adjust PWM if nedded */
+		if(pwm != old_pwm){
+			ioctl(fdpwm, GPIOPWM_RTIOC_CHANGE_DUTY_CYCLE, pwm);
+			old_pwm = pwm;
+		}
+
+		/* keep statistics */
+		update_statistics(ctr, (now - tstart)/DIV_TO_MS, (rt_timer_read()-now)/DIV_TO_US, ((long int)now - (long int)tlast - LOOP_PERIOD)/DIV_TO_US, value, pwm);
+		
+		tlast = now;		
+
+		rt_task_wait_period(NULL);
+	}
+
+	close(fdspi);
+	close(fdpwm);
+}
+
+
+/*Print statistics */
+void print_results(){
+	int i;
+
+	printf("Fastest Loop(us)\tSlower Loop(us)\t\tBetter Jitter(us)\tWorst Jitter(us)\n");
+	printf("%.2f\t\t\t%.2f\t\t\t%.2f\t\t\t%.2f\n\n", time_cases[FASTEST_LOOP], time_cases[SLOWER_LOOP], time_cases[BETTER_JITTER], time_cases[WORST_JITTER]);
+
+	printf("Loop count\trun time(ms)\tloop time(us)\tjitter(us)\tanalog\t\tpwm\n");
+
+	for(i=0; i<TABLE_SIZE; i++){
+		printf("%d\t\t%.2f\t%.2f\t\t%.2f\t\t%d\t\t%d\n", (int)loop_info[i][0], loop_info[i][1], loop_info[i][2], loop_info[i][3], (int)loop_info[i][4], (int)loop_info[i][5]);
+	}
+
+}
+
+
 int main(int argc, char *argv[]){
+	RT_TASK loop_task;
 	char str[20];
 	int ret;
 
-	//Create the real time task
+	memset(loop_info, 0, sizeof(loop_info));
+
+	/* Create the real time task */
 	sprintf(str, "spi_loop");
 	ret = rt_task_create(&loop_task, str, 1024, 99, 0);
 	if(ret != 0){
@@ -197,17 +284,19 @@ int main(int argc, char *argv[]){
 		exit(0);
 	}
 
-	//Since task starts in suspended mode, start task
+	/* Since task starts in suspended mode, start task */
 	rt_task_start(&loop_task, &loop_task_proc, 0);
 
 	printf("press key to finish...\n");
 
-	//Wait for Ctrl-C
+	/* Wait for ENTER */
 	getchar();
 
-	printf("finished...");
+	finish = 0;
 
-	close(fd);
+	printf("finished...\n");
+
+	print_results();
 
 	return ret;
 }
